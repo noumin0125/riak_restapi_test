4a0b0ee29aaf00e9458bf510fd98928f1e470116
noumin0125 <noumin0125@gmail.com>
2017-10-09 02:39:53 +0900

    update configure

--- app/main.rb
+++ app/main.rb
@@ -1,44 +1,48 @@
 #!/usr/local/bin/ruby
 
 require 'sinatra'
-require "sinatra/reloader"
+require 'sinatra/reloader'
 require 'net/http'
 require 'json'
-require "uri"
+require 'uri'
+
+configure do
+  BASE_URL = 'http://localhost:8098'.freeze
+  BASE_BUCKETS = 'buckets/users'.freeze
+end
 
 get '/' do
-  @result = JSON.parse(Net::HTTP.get(URI.parse('http://localhost:8098/buckets/users/keys?keys=true')))
+  @result = JSON.parse(Net::HTTP.get(URI.parse("#{BASE_URL}/#{BASE_BUCKETS}/keys?keys=true")))
   erb :index
 end
 
 get '/user/detail/:name' do
   @name = params[:name]
-  @result = JSON.parse(Net::HTTP.get(URI.parse("http://localhost:8098/buckets/users/keys/#{@name}")))
+  @result = JSON.parse(Net::HTTP.get(URI.parse("#{BASE_URL}/#{BASE_BUCKETS}/keys/#{@name}")))
   erb :user
 end
 
-post "/user/add" do
+post '/user/add' do
   payload =  {
-    "team" => params[:team]
+    'team' => params[:team]
   }
-  uri = URI.parse("http://localhost:8098/buckets/users/keys/#{params[:name]}?returnbody=true")
+  uri = URI.parse("#{BASE_URL}/#{BASE_BUCKETS}/keys/#{params[:name]}?returnbody=true")
   http = Net::HTTP.new(uri.host, uri.port)
   req = Net::HTTP::Post.new(uri.request_uri)
-  req["Content-Type"] = "application/json"
+  req['Content-Type'] = 'application/json'
   req.body = payload.to_json
   http.request(req)
   redirect '/'
 end
 
 get '/user/delete/:name' do
-  uri = URI.parse("http://localhost:8098/buckets/users/keys/#{params[:name]}")
+  uri = URI.parse("#{BASE_URL}/#{BASE_BUCKETS}/keys/#{params[:name]}")
   http = Net::HTTP.new(uri.host, uri.port)
   req = Net::HTTP::Delete.new(uri.request_uri)
   http.request(req)
   redirect '/'
 end
 
-__END__
 __END__
 
 @@ layout
@@ -53,7 +57,7 @@ __END__
 <%= yield %>
 </body>
 
-@@index
+@@ index
 <h3>user list</h3>
 <% @result["keys"].each do |value| %>
 <li><a href="/user/detail/<%= value %>"><%= value %></a>:<a href="/user/delete/<%= value %>">x</a></li>
@@ -66,7 +70,6 @@ __END__
   <p><input type= "submit" value="add"></p>
 </form>
 
-@@user
 @@ user
 name:<%= @name %><br>
 team:<%= @result['team'] %><br>
